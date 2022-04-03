dd36d573bd2efec9851fc83af439d10e6f79b73f
Ryan <ryanmturnbull@gmail.com>
2022-04-03 13:09:17 +1000

    Added tabs and added a func to register a command suggestion

--- shared.lua
+++ shared.lua
@@ -1,29 +1,38 @@
 RiotMode = false
 Blackout = false
 
-RegisterCommand('riot', function(source, args, rawCommand)
-	if RiotMode == false then
-		RiotMode = true
-	else
-		RiotMode = false
-	end
-end, true)
+	RegisterCommand('riot', function(source, args, rawCommand)
+		if RiotMode == false then
+			RiotMode = true
+		else
+			RiotMode = false
+		end
+	end, true)
 
-RegisterCommand('blackout', function(source, args, rawCommand)
-	if Blackout == false then
-		Blackout = true
-	else
-		Blackout = false
-	end
-end, true)
+	RegisterCommand('blackout', function(source, args, rawCommand)
+		if Blackout == false then
+			Blackout = true
+		else
+			Blackout = false
+		end
+	end, true)
 
 -- CREDIT: https://github.com/TFNRP/framework
-function RegisterFrameworkCommand (name, handler, restricted)
-	if type(name) == 'table' then
-		for _, c in ipairs(name) do
-			RegisterFrameworkCommand(c, handler, restricted)
+	function RegisterFrameworkCommand (name, handler, restricted)
+		if type(name) == 'table' then
+			for _, c in ipairs(name) do
+				RegisterFrameworkCommand(c, handler, restricted)
+			end
+		else
+			RegisterCommand(name, handler, restricted)
+		end
+	end
+	function RegisterCommandSuggestion(command, description, parameters, client)
+	if type(command) == 'table' then
+		for _, c in ipairs(command) do
+		RegisterCommandSuggestion(c, description, parameters)
 		end
 	else
-		RegisterCommand(name, handler, restricted)
+		TriggerEvent('chat:addSuggestion', '/' .. command, description, parameters)
 	end
-end
+	end
