8c9126d62d6d6240cf443c04278bff5486a6207b
Boz <ryanmturnbull@gmail.com>
2022-05-09 22:42:23 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,13 +3,13 @@ Blackout = false
 
 RichPresence = {
     Enabled = true,
-    AppID = "858521709236453416",
-    AssetID = "sa",
+    AppID = "", -- ID of the Discord Application you wish to use. Leave empty to use the default FiveM one.
+    AssetID = "", -- Requires you to have set an AppID and added rich presence assets.
     Text = {
         Line1 = "Cfx.re Development Kit",
         Line2 = "Experimenting a little",
     },
-    -- TO CHANGE PICTURES AND LINKS YOU WILL HAVE TO GO TO CLIENT.LUA FOR NOW!
+    -- BELOW THIS LINE IS NOT FUNCTIONING! TO CHANGE PICTURES AND LINKS YOU WILL HAVE TO GO TO CLIENT.LUA FOR NOW!
     Button1 = { 
         Text = "Connect to the server", -- NOT YET FUNCTIONING
         Link = "fivem://connect/m633od", -- NOT YET FUNCTIONING
