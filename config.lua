057b062b9ca0bf642ca33bba58e2e9baf2eb24e5
Boz <ryanmturnbull@gmail.com>
2022-05-21 17:31:18 +1000

    Added ability to set up blips

--- config.lua
+++ config.lua
@@ -23,5 +23,43 @@ RichPresence = {
 	Button2 = {
 		Text = "See our website and servers list", -- NOT YET FUNCTIONING
 		Link = "https://www.policingmp.net", -- NOT YET FUNCTIONING
-	}
+	},
+}
+
+-- Map Blips
+blips = {
+	{
+		type = "Coffee Shop", -- Type of location, best to keep this consistent with others of the same type.
+		id = 76,  -- Blip ID to display (https://docs.fivem.net/docs/game-references/blips/)
+		-- Coordinates
+		x = -1281.38, 
+		y = -1129.33, 
+		z = 6.91, 
+		name = "Bean Machine", -- Name of location
+		image = "beanmachine_vespucci",  -- Name of 256x128 image in mapimages.ytd to use 
+		address = "1/8106 Magellan Avenue, Los Santos, SA", -- Address of location
+		description = "Rainier-based coffeehouse chain known for its signature roasts, light bites and WiFi availability.", -- Description of location. Leave empty to igmore.
+		lowpriority = true, -- Setting this to false will make it appear on the minimap at all times, even if it's not visible there.
+		--[[ THIS SECTION IS A WORK IN PROGRESS AND DOESN'T WORK!
+			Check1 = { Title = "Dine-in", Value = 79, },
+			Check2 = { Title = "Drive-through", Value = 80, },
+			Check3 = { Title = "Delivery", Value = 79, },
+		]]
+		website = "beanmachinecoffee.com",
+		phone = "+1 310-555-0134",
+	},
+	{
+		type = "Coffee Shop",
+		id = 76,
+		x = -477.11, 
+		y = 1116.29, 
+		z = 320.23, 
+		name = "Bean Machine",
+		image = "beanmachine_galileo",
+		address = "Galileo Observatory, Los Santos, SA",
+		description = "Rainier-based coffeehouse chain known for its signature roasts, light bites and WiFi availability.",
+		lowpriority = true,
+		website = "beanmachinecoffee.com",
+		phone = "+1 323-555-0231",
+	},
 }
