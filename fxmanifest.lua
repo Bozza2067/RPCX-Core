9cc88203e747db16bbb9517afa69c84eb6c1861a
Boz <ryanmturnbull@gmail.com>
2022-06-29 01:11:44 +1000

    Revert "Removed map (spawnpoints) functionality"
    This reverts commit 4554f9931ac31ce376984def71f5debc7a8a662d.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,9 @@ author 'Boz'
 description 'PolicingMP Core'
 version '1.0.0'
 
+resource_type 'map' { gameTypes = { ['basic-gamemode'] = true } }
+map 'map.lua'
+
 client_script 'client.lua'
 server_script 'server.lua'
 shared_script 'shared.lua'
