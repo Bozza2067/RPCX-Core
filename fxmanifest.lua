cd57015fe653cc2ea8872abaaaec8295d4e30225
Boz <ryanmturnbull@gmail.com>
2022-07-02 17:41:39 +1000

    Removed spawnpoints

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,6 @@ author 'Boz'
 description 'PolicingMP Core'
 version '1.0.0'
 
-resource_type 'map' { gameTypes = { ['basic-gamemode'] = true } }
-map 'map.lua'
-
 client_script 'client.lua'
 server_script 'server.lua'
 shared_script 'shared.lua'
