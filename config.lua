36cc0b85244d79f954ba6e180abe3194e8b9f9a9
Boz <ryanmturnbull@gmail.com>
2022-11-01 16:06:42 +1100

    mfw i keep committing to core instead of the server itself
    fixed security flaw

--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
 HideMinimapOnFoot = false
 BetterFlashlightInstalled = true -- Set this to true when BetterFlashlight is installed (https://miraf.tebex.io/package/4941283)
-webhook = 'https://discord.com/api/webhooks/1036868415253643344/PczOKXUXlp0KXkShi7u3lLP_j4_j1BWykgFGGzlOoG6IOX8S0RhXppVhOK0nifP-_Dji'
+webhook = '' -- your webhook here
