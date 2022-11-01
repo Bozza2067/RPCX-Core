42487aaa16b2f6f2d187d6d5cd7ea49ef24ec59e
Boz <ryanmturnbull@gmail.com>
2022-11-01 16:05:33 +1100

    Fix security flaw

--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
 HideMinimapOnFoot = false
 BetterFlashlightInstalled = true -- Set this to true when BetterFlashlight is installed (https://miraf.tebex.io/package/4941283)
-webhook = 'https://discord.com/api/webhooks/1031516867262677023/BdU33sIivfnC0UX1eMIg2cUSxRetrhZhCqtoYGGbEZs3cViDYdJ1KeVAvRN2zmfXyUP6'
+webhook = 'https://discord.com/api/webhooks/1036868415253643344/PczOKXUXlp0KXkShi7u3lLP_j4_j1BWykgFGGzlOoG6IOX8S0RhXppVhOK0nifP-_Dji'
